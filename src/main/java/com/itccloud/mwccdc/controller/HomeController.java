b6d6a5b897ac40638379400aced96ded40f7a77f
William H. <141768348+william00193@users.noreply.github.com>
2024-09-26 16:05:02 -0500

    Add files via upload

--- src/main/java/com/itccloud/mwccdc/controller/HomeController.java
+++ src/main/java/com/itccloud/mwccdc/controller/HomeController.java
@@ -1,14 +1,40 @@
 package com.itccloud.mwccdc.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 	
 	@GetMapping("/")
-	public String index() {
+	public String index(Model model) {
+		
+		model.addAttribute("message", "Hello William");
+		
+		User user = new User("Output from User Variable defined in index function");
+		
+		model.addAttribute("user", user);
 		return "index-form";
+		
 	}
+	
+	@GetMapping("/advance")
+	public String advance(Model model) {
+		
+		List<User> users1 = new ArrayList<User>();
+		users1.add(new User("New User 1"));
+		users1.add(new User("New User 2"));
+		
+		model.addAttribute("users1", users1);
+	
+		return "advance-form";
+		
+	}
+	
+	
 
 }
